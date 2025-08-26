dddfc1b515042c52fb661024b43517bb5937314d
Rohit8426 <your_email@example.com>
2025-08-26 20:35:20 +0530

    updated

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 // Single file upload controller
 exports.uploadFile = (req, res) => {
+  console.log("req.file:", req.file);
+  
   if (!req.file) {
     return res.status(400).json({ error: "No file uploaded" });
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -19,5 +19,5 @@ module.exports = (app) => {
   app.use("/api", emailRoutes);
   app.use("/api", documentRoutes);
   app.use("/api", headerBlockRoutes);
-  app.use("/upload", upload);
+  app.use("/api/upload", upload);
 };
