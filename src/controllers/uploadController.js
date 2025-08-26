26dd5ed669cb4d94d196e5a097a5e8bacfae72e6
Rohit8426 <your_email@example.com>
2025-08-26 17:35:03 +0530

    updated res frolm upload

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -6,7 +6,7 @@ exports.uploadFile = (req, res) => {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
-  const fileUrl = `${process.env.BASE_URL || "http://localhost:5000"}/uploads/${req.file.filename}`;
+  const fileUrl = `${process.env.BASE_URL || "http://13.204.3.50"}/uploads/${req.file.filename}`;
 
   res.json({
     success: true,
