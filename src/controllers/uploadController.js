632bb16417aba4fc07495beab6d5e535efd296c6
Rohit8426 <your_email@example.com>
2025-08-26 17:38:43 +0530

    C:/Program Files/Git/api added

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -6,7 +6,7 @@ exports.uploadFile = (req, res) => {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
-  const fileUrl = `${process.env.BASE_URL || "http://13.204.3.50"}/uploads/${req.file.filename}`;
+  const fileUrl = `${process.env.BASE_URL || "http://13.204.3.50"}/api/uploads/${req.file.filename}`;
 
   res.json({
     success: true,
